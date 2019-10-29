9a76ade3ca44f5f03b40538d898e056118cca849
linzhifeng <249284465@qq.com>
2019-10-29 17:05:28 +0800

    test

---
+++ src/helloworld/HelloWorld.java
@@ -0,0 +1,15 @@
+package helloworld;
+
+/**
+ * @Description 
+ * @date  2019年10月29日
+ * @author  lzf
+ * @version  V1.0
+ */
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
